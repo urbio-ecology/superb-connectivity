4aebd705b1f84f892714738ec4e1b60f1d7739ae
njtierney <nicholas.tierney@gmail.com>
2025-09-23 12:31:47 +1000

    remove unused clean_inter function

--- R/clean.R
+++ R/clean.R
@@ -13,21 +13,3 @@ clean <- function(spatial_data, ...) {
   simplified <- sf::st_simplify(unioned, dTolerance = 1)
   simplified
 }
-
-# clean membership to account for when some fragments appear in no buffered
-# areas, or some appear in more than one.
-clean_inter <- function(membership_vector) {
-  # check for empty vectors
-  if (length(membership_vector) == 0) {
-    membership_vector <- NA
-  }
-
-  if (length(membership_vector) > 1) {
-    membership_vector <- membership_vector[1]
-    warning(
-      "One of the fragments occurs in more than one buffered area, ",
-      "so has been assigned to the first area in the list"
-    )
-  }
-  membership_vector
-}

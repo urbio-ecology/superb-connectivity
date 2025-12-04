91494bb4ed8d247d42823d64bed1d0cdfbb47b51
njtierney <nicholas.tierney@gmail.com>
2025-12-04 17:31:29 +1100

    remove shiny-run.R

--- app/shiny-run.R
+++
@@ -1,8 +0,0 @@
-# enable auto-reload during development
-options(shiny.autoreload = TRUE)
-
-# run the app from the app directory
-shiny::runApp(
-  appDir = here::here("app"),
-  launch.browser = TRUE
-)

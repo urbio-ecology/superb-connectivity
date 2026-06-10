d208b64d89c3993af8cb008ac65840f2e09272f1
njtierney <nicholas.tierney@gmail.com>
2026-06-10 11:43:51 +1000

    add tests to ensure it works with dplyr

---
+++ tests/testthat/test-dplyr-compatible.R
@@ -0,0 +1,16 @@
+test_that("patch_connectivity class is compatible with dplyr", {
+  # summarise_connectivity.patch_area
+  areas <- habitat_connectivity(
+    habitat = example_habitat(),
+    interpatch_distance = 8,
+    barrier = example_barrier(),
+    species = "Blue-tongued Lizard",
+  )
+
+  library(dplyr)
+  expect_snapshot(areas |> filter(area > 4000))
+  expect_snapshot(areas |> filter(area > 1000))
+  expect_snapshot(areas |> slice(1:10))
+  expect_snapshot(areas |> select(-area) |> head())
+  expect_snapshot(areas |> select(-patch_id) |> head())
+})
